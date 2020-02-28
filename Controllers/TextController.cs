cabb89e9f6be02c01b6496272a33fc688d9f3e73
Matthew Ross <matthew.douglas.ross@gmail.com>
2020-02-28 16:19:09 -0500

    Add project files.

---
+++ Controllers/TextController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Twilio;
+using Twilio.AspNet.Core;
+using Twilio.AspNet.Common;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.TwiML;
+using Twilio.Types;
+
+namespace TextToEmail.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TextController : TwilioController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            var from = Request.Form["From"];
+            var body = Request.Form["Body"];
+            var emailer = new Emailer();
+            var message = await emailer.Send(from, body);
+            var response = $"<Response><Message>{message}</Message></Response>";
+            return Content(response, "text/xml");
+        }
+
+    }
+}
---
+++ Emailer.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace TextToEmail
+{
+    public class Emailer
+    {
+        private readonly string _sendGridApiKey;
+        public Emailer()
+        {
+            _sendGridApiKey = Environment.GetEnvironmentVariable("SENDGRID_KEY");
+        }
+        private class Email
+        {
+            public Email(string from, string body)
+            {
+                {
+                    To = new EmailAddress("matthew.douglas.ross@gmail.com", "spuuun");
+                    From = new EmailAddress("matthew.douglas.ross@gmail.com", "Text to Email App");
+                    Subject = $"A new text message has been sent from {from}";
+                    HtmlContent = $"<strong>{body}<strong>";
+                    PlainTextContent = body;
+                };
+            }
+            public EmailAddress From { get; set; }
+            public EmailAddress To { get; set; }
+            public string Subject { get; set; }
+            public string PlainTextContent { get; set; }
+            public string HtmlContent { get; set; }
+        }
+
+        public async Task<string> Send(string from, string body)
+        {
+            var email = new Email(from, body);
+
+            var response = await SendEmail(email);
+
+            var message = "Your message could not be processed at this time. Please try again later.";
+
+            if (response.StatusCode == HttpStatusCode.Accepted)
+            {
+                message = "Thank you for your message, someone will be in touch soon!";
+            }
+            return message;
+        }
+
+        private async Task<Response> SendEmail(Email email)
+        {
+            var client = new SendGridClient(_sendGridApiKey);
+            var msg = MailHelper.CreateSingleEmail(
+                email.From, email.To, email.Subject, email.PlainTextContent, email.HtmlContent);
+            var response = await client.SendEmailAsync(msg);
+            return response;
+        }
+    }
+}
+
